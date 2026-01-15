2493715287e73dc67ffa368c9463c1db28bc5f39
evansn05 <evansn05@stirlingschools.net>
2026-01-15 16:57:45 +0000

    fixed error in 1-Variables.cpp

--- idk self made lessons maybe/1-Variables.cpp
+++ idk self made lessons maybe/1-Variables.cpp
@@ -3,8 +3,8 @@
 #include <string>
 
 int age = 17; // integer
-string name = "Nathan"; // string, if you choose not to do "#include <string>" you need to do "std::string" instead of just "string"
+std::string name = "Nathan"; // string, if you choose not to do "#include <string>" you need to do "std::string" instead of just "string"
 double price = 9.80; // floats, reals, decimal numbers, depends what you wanna call them
-bool valid = True; // standard booleans
+bool valid = true; // standard booleans
 
 // when creating a variable, you need to define at its instantiation what variable type it is
